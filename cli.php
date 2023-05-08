8839d6e6b33b3404bdf0517becacf621ba2219fe
Kosmos258 <103069422+Kosmos258@users.noreply.github.com>
2023-05-08 17:57:25 +0300

    HW 4

---
+++ cli.php
@@ -0,0 +1,15 @@
+<?php
+
+use GeekBrains\LevelTwo\Blog\Commands\Arguments;
+use GeekBrains\LevelTwo\Blog\Commands\CreateUserCommand;
+
+$container = require __DIR__ . '/bootstrap.php';
+
+try {
+
+    // При помощи контейнера создаём команду
+    $command = $container->get(CreateUserCommand::class);
+    $command->handle(Arguments::fromArgv($argv));
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
