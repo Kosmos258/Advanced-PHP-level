11ac23f8ae8a58ccbe4260ec0431ba7751d661ae
Kosmos258 <103069422+Kosmos258@users.noreply.github.com>
2023-05-31 17:27:22 +0300

    Add homework 6

---
+++ src/Blog/Repositories/LikesRepository/SqliteLikesRepository.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace GeekBrains\LevelTwo\Blog\Repositories\LikesRepository;
+
+use GeekBrains\LevelTwo\Blog\Exceptions\InvalidArgumentException;
+use GeekBrains\LevelTwo\Blog\Exceptions\LikesNotFoundException;
+use GeekBrains\LevelTwo\Blog\Like;
+use GeekBrains\LevelTwo\Blog\UUID;
+use GeekBrains\LevelTwo\Blog\Exceptions\LikeAlreadyExists;
+use Psr\Log\LoggerInterface;
+
+class SqliteLikesRepository implements LikesRepositoryInterface
+{
+    private \PDO $connection;
+    private LoggerInterface $logger;
+
+    public function __construct(\PDO $connection, LoggerInterface $logger)
+    {
+        $this->connection = $connection;
+        $this->logger = $logger;
+    }
+
+    public function save(Like $like): void
+    {
+        $statement = $this->connection->prepare('
+            INSERT INTO likes (uuid, user_uuid, post_uuid)
+            VALUES (:uuid, :user_uuid, :post_uuid)
+        ');
+        $statement->execute([
+            ':uuid' => (string)$like->uuid(),
+            ':user_uuid' => (string)$like->getUserId(),
+            ':post_uuid' => (string)$like->getPostId(),
+        ]);
+
+        $this->logger->info("Like created: {$like->uuid()}");
+    }
+
+    /**
+     * @throws LikesNotFoundException
+     * @throws InvalidArgumentException
+     */
+    public function getByPostUuid(UUID $uuid): array
+    {
+        $statement = $this->connection->prepare(
+            'SELECT * FROM likes WHERE post_uuid = :uuid'
+        );
+
+        $statement->execute([
+            'uuid' => (string)$uuid
+        ]);
+
+        $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+        if (!$result) {
+            throw new LikesNotFoundException(
+                'No likes to post with uuid = : ' . $uuid
+            );
+            $message = "Cannot find like: $$uuid";
+            $this->logger->warning($message);
+            throw new LikesNotFoundException($message);
+        }
+
+        $likes = [];
+        foreach ($result as $like) {
+            $likes[] = new Like(
+                uuid: new UUID($like['uuid']),
+                post_id: new UUID($like['post_uuid']),
+                user_id: new UUID($like['user_uuid']),
+            );
+        }
+
+        return $likes;
+    }
+
+    /**
+     * @throws LikeAlreadyExists
+     */
+    public function checkUserLikeForPostExists($postUuid, $userUuid): void
+    {
+        $statement = $this->connection->prepare(
+            'SELECT *
+            FROM likes
+            WHERE 
+                post_uuid = :postUuid AND user_uuid = :userUuid'
+        );
+
+        $statement->execute(
+            [
+                ':postUuid' => $postUuid,
+                ':userUuid' => $userUuid
+            ]
+        );
+
+        $isExisted = $statement->fetch();
+
+        if ($isExisted) {
+            throw new LikeAlreadyExists(
+                'The users like for this post already exists'
+            );
+        }
+    }
+}
